2839e6cfc003e7acfc9a696b4f834f21fe1f38e4
junki-stevekim <junkistevekim@gmail.com>
2022-12-22 17:12:57 +0900

    lv0- 두수의 몫 구하기

--- Programmers-lv0/몫구하기.js
+++ Programmers-lv0/몫구하기.js
@@ -1,6 +1,7 @@
 //정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
-//걸린시간:10초
 
+//걸린시간:10초
+// num1과 num2의 나눈값을 parseInt로 정수화시켜준다
 function solution(num1, num2) {
     var answer = parseInt(num1/num2);
     return answer;
