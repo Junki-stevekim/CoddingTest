c0e2a5c9221dfdf4c1bfba23a1bb2ed930277f31
Junki <junkistevekim@gmail.com>
2024-02-10 14:11:52 +0900

    LV2-N-Queen

---
+++ Programmers-lv2/N-Queen.js
@@ -0,0 +1,48 @@
+// 문제 설명
+// 가로, 세로 길이가 n인 정사각형으로된 체스판이 있습니다. 체스판 위의 n개의 퀸이 서로를 공격할 수 없도록 배치하고 싶습니다.
+
+// 예를 들어서 n이 4인경우 다음과 같이 퀸을 배치하면 n개의 퀸은 서로를 한번에 공격 할 수 없습니다.
+
+// Imgur
+// Imgur
+
+// 체스판의 가로 세로의 세로의 길이 n이 매개변수로 주어질 때, n개의 퀸이 조건에 만족 하도록 배치할 수 있는 방법의 수를 return하는 solution함수를 완성해주세요.
+
+// 제한사항
+// 퀸(Queen)은 가로, 세로, 대각선으로 이동할 수 있습니다.
+// n은 12이하의 자연수 입니다.
+
+function solution(n) {
+    const board = new Array(n).fill(-1)
+    let ret = 0
+    
+    const canCollocate = (board,col,row) => {
+        
+        for (let i = 0 ; i < col ; i++) {
+            if (board[i]===row) return false
+            if (Math.abs(col-i) === Math.abs(row - board[i])) return false
+        }
+        return true
+    }
+    
+    const dfs = (col,board) => {
+        
+        const tmpBoard = [...board]
+        if (col === n) ret ++
+        
+        
+        for (let row = 0 ; row < n ; row++){
+            
+            if (canCollocate(tmpBoard,col,row)) {
+                tmpBoard[col] = row
+                dfs(col+1,[...tmpBoard])
+            }
+        }
+    }
+
+   
+    dfs(0,board)
+        
+    return ret
+    
+}
